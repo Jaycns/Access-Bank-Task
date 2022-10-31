d83dfb24db62a28f8aea35a8a1bd6f85ce07fc36
Jaycn <okonkwojude69@gmail.com>
2022-10-31 08:45:16 +0100

    card added

--- src/pages/application.js
+++ src/pages/application.js
@@ -4,7 +4,13 @@ import Logo from "../images/logo.png";
 import { FirstSection, Footer } from "../styles/homeStyle";
 import logobg from "../images/Union.webp";
 import Background from "../images/blurred-bg.png";
-import { Header, Before } from "../styles/apiStyles";
+import { Header, Before, Mainer } from "../styles/apiStyles";
+import dan from "../images/products/dan.webp";
+import dar from "../images/products/dar.webp";
+import dani from "../images/products/dani.webp";
+import dmi from "../images/products/dmi.webp";
+import frez from "../images/products/frez.webp";
+import isa from "../images/products/isa.webp";
 
 function Application() {
   return (
@@ -21,6 +27,58 @@ function Application() {
             </p>
           </div>
         </Header>
+        <Mainer className="mainer">
+          <div className="container">
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={dan} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={dar} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={dani} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={isa} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={dmi} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="line"></div>
+              <div className="profile">
+                <div className="img">
+                  <img src={frez} alt="dan" loading="lazy" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Mainer>
         <Footer>
           <img src={Logo} alt="Logo" />
         </Footer>
